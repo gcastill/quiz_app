a569008b72eb7434c64933e8768d006ba3c6c9c7
Gustavo Castillo <gustavo.castillo@binance.us>
2025-08-06 19:51:34 -0500

    Set up basic quiz app structure with start and questions screens.

---
+++ lib/questions_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _QuestionsScreenState();
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("QuestionsScreen");
+  }
+}
